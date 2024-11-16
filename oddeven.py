2ce05b066c2aab984cb95e83ba691303ae824366
Fathima Fahira <152597551+Fahira-Fathima@users.noreply.github.com>
2024-11-16 12:49:50 +0100

    Add files via upload
    initial commit

---
+++ oddeven.py
@@ -0,0 +1,20 @@
+# program for determining odd or even by gettiong an input and dealing with errors
+print( "enter the number : ")
+try:
+    number = int(input())
+except:
+    print("only number allowed")
+else:
+    if number % 2 ==0:
+        print("number is even")
+    else:
+        print("number is odd")
+ '''try:
+    number = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input! Please enter a valid number.")
+else:
+    if number % 2 == 0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
